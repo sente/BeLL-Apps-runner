76707eca7115ca5cd9337613d46403422c39f0d7
Stuart Powers <stuart.powers@gmail.com>
2016-07-13 02:14:56 -0400

    Add initial model schemas

---
+++ schema/run_this_code_in_the_console.js
@@ -0,0 +1,37 @@
+(function(console){
+
+    console.save = function(data, filename){
+
+        if(!data) {
+            console.error('Console.save: No data')
+            return;
+        }
+
+        if(!filename) filename = 'console.json'
+
+        if(typeof data === "object"){
+            data = JSON.stringify(data, undefined, 4)
+        }
+
+        var blob = new Blob([data], {type: 'text/json'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+    }
+})(console);
+
+
+for (var mdl in App.Models) {
+    model = new App.Models[mdl];
+    for (attr in model) {
+        if (attr === 'schema') {
+            //console.log(JSON.stringify(model.schema,null,2));
+            console.save(model.schema,mdl + ".schema.json");
+        }
+    }
+}
