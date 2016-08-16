ce2cbcc832483aea1b835c0d2b24145fbd271c25
Stuart Powers <stuart.powers@gmail.com>
2016-08-16 15:45:40 -0400

    get_ports.sh tweak

--- scripts/get_ports.sh
+++ scripts/get_ports.sh
@@ -1,1 +1,1 @@
-ssh root@hammock cat /root/oleproxy/server.js  | grep '"http' |cut -f4 -d'"' | sed 's/\/apps.*//'
+ssh root@hammock cat /root/oleproxy/server.js  | grep '"http' |cut -f4 -d'"' | sed 's/\/apps.*//' | grep -v 192.168
