a56f6cf3ac3bbf5a06baaebc98403cf8ec772400
Stuart Powers <stuart.powers@gmail.com>
2017-02-09 03:59:00 -0500

    ports_and_versions.sh

---
+++ scripts/ports_and_versions.sh
@@ -0,0 +1,9 @@
+echo -ne "Host,Version\n"
+
+ssh root@hammock cat /root/oleproxy/server.js | grep '"http' | cut -f4 -d'"' | sed 's/\/apps.*//' | grep -v 192.168 | while read host; do
+    version="$(curl -s ${host}/configurations/_all_docs?include_docs=true| jq  '.rows[] | .doc | .version' | tr -d '"')";
+
+    echo -ne "$host,$version\n"; 
+done
+
+
